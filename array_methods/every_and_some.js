49a54276cd03f186bf16ddb54e8bb52262a573c4
Vladislav Pestsov <pestsov.js@gmail.com>
2022-04-21 20:20:57 +0300

    every and some methods

---
+++ array_methods/every_and_some.js
@@ -0,0 +1,36 @@
+const _ = require("lodash");
+
+const users = [
+  {
+    id: 1,
+    name: "John",
+    isActive: true,
+    likes: 110,
+  },
+  {
+    id: 2,
+    name: "Jack",
+    isActive: false,
+    likes: 30,
+  },
+];
+
+// some. Вернул true поскольку есть хоть один активный пользователь
+const isSomeActive = _.some(users, function (user) {
+  return user.isActive === true;
+});
+
+console.log("isSomeActive", isSomeActive);
+
+// some с сахаром. Вернул true поскольку есть хоть один активный пользователь
+const isSomeActive2 = _.some(users, { isActive: true });
+console.log("isSomeActive2", isSomeActive2);
+
+// every. Вернул true поскольку не все пользователи активны 
+const isAllActive = _.every(users, function (user) {
+  return user.isActive === true;
+});
+
+console.log("isAllActive", isAllActive);
+
+// some. Вернул true поскольку есть хоть один активный пользователь
--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // require("./work_with_array/map");
 // require("./array_methods/filter");
 // require("./array_methods/find");
-require("./array_methods/killed_elements");
-
+// require("./array_methods/killed_elements");
+require("./array_methods/every_and_some");
